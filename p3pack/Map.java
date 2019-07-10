047e1461b306552177f4ba7d6de182953b4a4b6a
ninjaChickin <51749659+ninjaChickin@users.noreply.github.com>
2019-07-10 15:23:10 -0400

    still working

--- p3pack/Map.java
+++ p3pack/Map.java
@@ -1,5 +1,47 @@
+import java.io.*;
+
 public class Map {
     protected Spot[][] floorplan;
     protected Thing[] things;
     protected java.io.PrintStream log;
+
+    Map(String filename, PrintStream log) throws IOException{
+            FileInputStream fyle = new FileInputStream(filename);
+            //loop through each char in fyle and add it to the floorplan
+            int i = 0;
+            int j = 0;
+            while (fyle.read() != -1){
+                if ((char)fyle.read() == '\n'){
+                    i++;
+                }else{
+                    try{
+                    floorplan[i][j] = Spot.valueOf(String.valueOf((char)fyle.read()));
+                    }catch (IllegalArgumentException iex) {
+                        log.append((char) fyle.read());
+                    }
+                    j++;
+                }
+
+        }
+    }
+    public boolean onMap(Coord c){
+        if (c.r < floorplan.length && c.r > -1){
+            if (c.c < floorplan[0].length && c.c > -1){return true;}
+        }
+        return false;
+    }
+    public Spot spotAt(Coord c){
+        if(onMap(c) == true){
+            return floorplan[c.r][c.c];
+        }
+        return null;
+    }
+    public int peopleRemaining(){
+        int count = 0;
+        for (int r = 0;r < floorplan.length;r++){
+            for (int c = 0; c < floorplan[r].length;c++){
+                if (floorplan[r][c] == Spot.)
+            }
+        }
+    }
 }
--- p3pack/Person.java
+++ p3pack/Person.java
@@ -1,2 +1,27 @@
-public class Person {
+import java.io.PrintStream;
+
+public abstract class Person extends Thing{
+    protected Direction facing;
+    protected Status status;
+
+    public Person(Coord loc, String repr, Map map, PrintStream log){
+        super(loc,repr,map,log);
+        facing = Direction.N;
+    }
+    public abstract Coord chooseMove();
+    @Override public void doAction(){
+
+    }
+    public boolean isSafe(){
+        if (status == Status.Safe){return true;}
+        return false;
+    }
+    public void die(){status = Status.Dead;}
+    @Override public void setLoc(Coord newLoc){
+        super.setLoc(newLoc);
+        if (map.spotAt(super.getLoc()) == Spot.Exit){
+            status = Status.Safe;
+            log.append("");
+        }
+    }
 }
